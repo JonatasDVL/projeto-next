81410a77e4c25c385c2e068f93e038c4b974957e
Jônatas Duarte <jonatasdvl@hotmail.com>
2024-04-08 14:06:22 -0300

    route test

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Sobre() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+      Sobre
     </main>
   );
 }
