dabd73cce2f7aeacb5f0891c6f6d6672e5efdca1
Jônatas Duarte <jonatasdvl@hotmail.com>
2024-04-09 15:09:38 -0300

    edit contador page

--- src/components/ButtonsCounts.tsx
+++ src/components/ButtonsCounts.tsx
@@ -1,14 +1,16 @@
 interface PropsButtonsCounts{
   addClassName?: string;
-  children: React.ReactNode;
+  len?: string;
+  children: React.ReactNode;  
   onClick: () => void;
 };
 
 export default function ButtonsCounts(props: PropsButtonsCounts) {
   const { addClassName, children, onClick } = props;
+  const len = props.len || "w-24 h-24";
 
   return (
-    <button className={`${addClassName} text-2xl text-gray-200 font-bold w-24 h-24 rounded-full hover:text-zinc-900 transform hover:scale-105 duration-200`} onClick={onClick}>
+    <button className={`${addClassName} text-2xl text-gray-200 font-bold ${len} rounded-full hover:text-zinc-900 transform hover:scale-105 duration-200`} onClick={onClick}>
       {children}
     </button>
   );
--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -8,6 +8,7 @@ interface PropsLinkButton {
 
 export default function LinkButton(props: PropsLinkButton) {
   const addClassName = props.addClassName || "";
+
   return (
     <Link className={`text-gray-100 font-semibold px-4 py-2 border-2 rounded-md flex justify-center items-center hover:text-zinc-900 hover:bg-gray-100 transform hover:scale-105 duration-200 ${addClassName}`}
       href={`/${props.urlPage}`}>
